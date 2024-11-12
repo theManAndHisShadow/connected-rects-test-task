71cad5277c189f7b09d7db57c6c174cf95c6389d
theManAndHisShadow <justemailpostbox@gmail.com>
2024-11-12 18:07:17 +0500

    [Core]: now 'PrimitiveShape' extends 'SynteticEventTarget'.

--- src/ts/classes/Primitive.ts
+++ src/ts/classes/Primitive.ts
@@ -1,14 +1,24 @@
-export default class PrimitiveShape implements GeometricPrimitive {
+import SynteticEventTarget from "./SynteticEventTarget";
+
+/**
+ * Базовый класс геометрического примитива. 
+ * Даёт начало для всех фигур в проекте.
+ */
+export default class PrimitiveShape extends SynteticEventTarget implements GeometricPrimitive {
     position: Point;
     size: Size;
     style: Style;
+    mouseover: boolean;
+    mouseout: boolean;
 
     constructor(params: GeometricPrimitive){
+        super();
+
         Object.assign(this, params);
-    }
 
-    moveTo(){
-
+        // Тут мы сохраняем 2 взаимно исключающих события, котоыре позволяют событиям mouseover и mouseout срабатывать корректно
+        this.mouseout = false;
+        this.mouseover = false;
     }
 
     renderAt(canvas: CanvasRenderingContext2D){
