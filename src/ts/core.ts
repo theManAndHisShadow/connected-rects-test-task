a43fa9f1b975c2d5948f2737ac224f90504a2521
theManAndHisShadow <justemailpostbox@gmail.com>
2024-11-12 15:17:06 +0500

    [Core]: added 'drawGrid()' function.

--- src/ts/core.ts
+++ src/ts/core.ts
@@ -44,3 +44,26 @@ export function drawRect(context: CanvasRenderingContext2D, params: Rect): Rect 
 
     return params;
 }
+
+export function drawGrid(context: CanvasRenderingContext2D, params: {gridSize: number, gridLineThickness: number, gridLineColor: string}): void{
+    const { width, height } = context.canvas;
+
+    context.strokeStyle = params.gridLineColor;
+    context.lineWidth = params.gridLineThickness;
+
+    // single 'beginPath'call - faster result
+    context.beginPath();
+
+    // draw a vertical and horizontal lines
+    for (let x = 0; x <= width; x += params.gridSize) {
+        context.moveTo(x, 0);
+        context.lineTo(x, height);
+    }
+
+    for (let y = 0; y <= height; y += params.gridSize) {
+        context.moveTo(0, y);
+        context.lineTo(width, y);
+    }
+
+    context.stroke();
+}
