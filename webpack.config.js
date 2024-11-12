b2da936f3bd86a4159e26a6cbaa83d0e312ae8b7
theManAndHisShadow <justemailpostbox@gmail.com>
2024-11-12 13:27:38 +0500

    [Project]: integrated new 'copy-webpack-plugin' to building process.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/ts/index.ts',
@@ -31,6 +32,15 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
+        }),
+
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/css'),
+                    to: path.resolve(__dirname, 'build/css'),
+                },
+            ],
         }),
     ],
 };
