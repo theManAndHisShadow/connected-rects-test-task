fa2aa1a0a84a26989048ce690dd2f191534a19de
theManAndHisShadow <justemailpostbox@gmail.com>
2024-11-12 17:00:11 +0500

    [Main logic]: updated 'index.ts' fie using new classes.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,72 +1,79 @@
-import { createCanvas, drawGrid, drawRect, fillLayer, getCoor as getColor } from "./core";
+import { drawGrid, getColor } from "./helpers";
+import InteractiveCanvas from "./classes/InteractiveCanvas";
+import RectangleShape from "./classes/Reactangle";
 
 // init project canvas object
-const canvas = createCanvas("#app-root", 500, 500);
+const canvas = new InteractiveCanvas("#app-root", 500, 500);
 
-// rendering backrgound layer once for better performance
-fillLayer(canvas.background, getColor('carbon'));
+// rendering background layer once for better performance
+canvas.background.fill(getColor('carbon'));
 
-drawGrid(canvas.background, {
+// render simple grid to backrgound
+drawGrid(canvas.background.context, {
     gridSize: 20,
     gridLineColor: 'rgba(255, 255, 255, 0.025)',
     gridLineThickness: 1,
+});
+
+ // creating base shapes
+ const rect1 = new RectangleShape({
+    size: {
+        width: 100, height: 80,
+    },
+
+    position: {
+        x: 30, y: 10,
+    },
+
+    style: {
+        fillColor: getColor('darkRed'),
+        borderColor: getColor('brightRed'),
+        borderThickness: 2,
+    },
+});
+
+const rect2 = new RectangleShape({
+    size: {
+        width: 100, height: 80,
+    },
+
+    position: {
+        x: 230, y: 110,
+    },
+
+    style: {
+        fillColor: getColor('darkBlue'),
+        borderColor: getColor('brightBlue'),
+        borderThickness: 2,
+    },
+});
+
+
+const rect3 = new RectangleShape({
+    size: {
+        width: 100, height: 80,
+    },
+
+    position: {
+        x: 150, y: 210,
+    },
+
+    style: {
+        fillColor: getColor('darkGreen'),
+        borderColor: getColor('brightGreen'),
+        borderThickness: 2,
+    },
 });
 
 // rendering foreground layer each frame
 const loop = () => {
     requestAnimationFrame(loop);
 
-    // make some basic actions
-    const rect1 = drawRect(canvas.foreground, {
-        size: {
-            width: 100, height: 80,
-        },
-
-        position: {
-            x: 30, y: 10,
-        },
-
-        style: {
-            fillColor: getColor('darkRed'),
-            borderColor: getColor('brightRed'),
-            borderThickness: 2,
-        },
-    });
-
-    const rect2 = drawRect(canvas.foreground, {
-        size: {
-            width: 100, height: 80,
-        },
-
-        position: {
-            x: 230, y: 110,
-        },
-
-        style: {
-            fillColor: getColor('darkBlue'),
-            borderColor: getColor('brightBlue'),
-            borderThickness: 2,
-        },
-    });
-
-
-    const rect3 = drawRect(canvas.foreground, {
-        size: {
-            width: 100, height: 80,
-        },
-
-        position: {
-            x: 150, y: 210,
-        },
-
-        style: {
-            fillColor: getColor('darkGreen'),
-            borderColor: getColor('brightGreen'),
-            borderThickness: 2,
-        },
-    });
+    rect1.renderAt(canvas.foreground.context);
+    rect2.renderAt(canvas.foreground.context);
+    rect3.renderAt(canvas.foreground.context);
 }
 
 loop();
 
-console.log(canvas);
+console.log(canvas, rect1);
