f4f860242cbdc4498eb39ba4bbaf6479b0b40230
theManAndHisShadow <justemailpostbox@gmail.com>
2024-11-15 17:33:45 +0500

    [Core]: added new 'port.reconnect()' method. Added some hotfixes.

--- src/ts/classes/Rectangle.ts
+++ src/ts/classes/Rectangle.ts
@@ -17,10 +17,11 @@ export default class RectangleShape extends PrimitiveShape implements Rectangle 
         Object.assign(this, params);
 
         this.ports = {
-            A: new ConnectionPort({ letter: 'A', parent: this, angle: 180, r: 3, x: this.position.x,  y: this.position.y + (this.size.height / 2) }),
-            B: new ConnectionPort({ letter: 'B', parent: this, angle: 90, r: 3, x: this.position.x + (this.size.width / 2),  y: this.position.y}),
-            C: new ConnectionPort({ letter: 'C', parent: this, angle: 0, r: 3, x: this.position.x + this.size.width,  y: this.position.y + (this.size.height / 2) }),
-            D: new ConnectionPort({ letter: 'D', parent: this, angle: 270, r: 3, x: this.position.x + (this.size.width / 2),  y: this.position.y + this.size.height }),
+            // Я вынес порты за границу прямоугольника используя 'this.style.margin' ввиду бага (пока не исправлено)
+            A: new ConnectionPort({ letter: 'A', parent: this, angle: 180, r: 3, x: this.position.x - this.style.maring,  y: this.position.y + (this.size.height / 2) }),
+            B: new ConnectionPort({ letter: 'B', parent: this, angle: 90, r: 3, x: this.position.x + (this.size.width / 2),  y: this.position.y - this.style.maring}),
+            C: new ConnectionPort({ letter: 'C', parent: this, angle: 0, r: 3, x: this.position.x + this.size.width + this.style.maring,  y: this.position.y + (this.size.height / 2) }),
+            D: new ConnectionPort({ letter: 'D', parent: this, angle: 270, r: 3, x: this.position.x + (this.size.width / 2),  y: this.position.y + this.size.height + this.style.maring}),
         };
     }
 
@@ -29,14 +30,17 @@ export default class RectangleShape extends PrimitiveShape implements Rectangle 
         let dy = this.position.y - newY;
 
 
-        // 
-        Object.values(this.ports).forEach(sidePort => {
-            sidePort.connectionPoint.point.x -= dx;
-            sidePort.connectionPoint.point.y -= dy;
+        // Логика проста:
+        // - прямоугольник знает, что у него поменялись координаты
+        // - прямоугольник знает свои порты
+        // - прямоугольник знает какой у него порт занят
+        // - а значит - вызываем перерисовку соеднения с учётом новых состояний (координат)
+        Object.values(this.ports).forEach(port => {
+            port.connectionPoint.point.x -= dx;
+            port.connectionPoint.point.y -= dy;
 
-           if(sidePort.connection !== null) {
-                // Вызываем обновление соединения при перемещении родительского прямоугольника
-                sidePort.connection.updatePath();
+           if(port.isBusy) {
+                port.reconnect();
            }
         });
 
