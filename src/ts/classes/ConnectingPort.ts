61fd3e95b6146ad6371349867c190cdaca9875b1
theManAndHisShadow <justemailpostbox@gmail.com>
2024-11-15 17:35:05 +0500

    [Core]: several important changes.
    Changelog:
    - fixed 'ConnectionPort.connectTo()' method;
    - added new 'ConnectionPort.reconnect()' method.

--- src/ts/classes/ConnectingPort.ts
+++ src/ts/classes/ConnectingPort.ts
@@ -7,14 +7,14 @@ export default class ConnectionPort {
     letter: string;
     isBusy: boolean;
     endPoint: null | ConnectionPort;
-    connection: ConnectionLine | null;
+    connection: ConnectionLine | null; 
 
-    constructor(params: { letter: string, x: number, y: number, r: number, parent: Rectangle, angle: number }) {
+    constructor(params: { letter: string, x: number, y: number, r: number, parent: Rectangle, angle: number}) {
         this.connectionPoint = {
             point: {x: params.x, y: params.y},
             angle: params.angle,
         }
-
+        
         this.letter = params.letter;
         this.parent = params.parent;
         this.r = params.r;
@@ -25,27 +25,37 @@ export default class ConnectionPort {
     }
 
     connectTo(endPoint: ConnectionPort): void{
+        // взаимно связываем эндопоинты
         this.endPoint = endPoint;
+        endPoint.endPoint = this;
+
         this.isBusy = true;
         endPoint.isBusy = true;
 
 
         this.connection = new ConnectionLine(this, this.endPoint);
         this.endPoint.connection = this.connection;
-        console.log(this.connection);
+    }
+
+
+    // Если есть необхоимость перерисовать соединение
+    // Новое соедниение учитывает новое состояние внешней среды (новые координаты прямоугольника и порта) и создаёт новый оптимальный путь
+    reconnect(){
+        this.connection = new ConnectionLine(this, this.endPoint);
+        this.endPoint.connection = this.connection;
     }
 
     renderAt(context: CanvasRenderingContext2D): void {
         if(this.connection !== null) {
-            // this.connection.renderAt(context);
+            this.connection.renderAt(context);
         }
 
         context.beginPath();
         context.arc(this.connectionPoint.point.x, this.connectionPoint.point.y, this.r, 0, 2 * Math.PI, false);
-        context.fillStyle = this.isBusy ? 'rgba(255, 255, 255, 0.3)' : this.parent.style.fillColor;
-        context.strokeStyle = this.isBusy ? 'rgba(255, 255, 255, 0.4)' : this.parent.style.borderColor;
-        context.fill();
+        context.fillStyle = this.isBusy ? 'rgba(125, 125, 125, 0.3)' : this.parent.style.fillColor;
+        context.strokeStyle = this.isBusy ? 'rgba(155, 155, 155, 0.4)' : this.parent.style.borderColor;
         context.closePath();
+        context.fill();
         context.stroke();
     }
 }
