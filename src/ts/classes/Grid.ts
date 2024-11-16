ec348185cfb234bbdd356f67f38eb9b93ffdb4eb
theManAndHisShadow <justemailpostbox@gmail.com>
2024-11-16 19:05:35 +0500

    [Core]: added near layer items grid points.

--- src/ts/classes/Grid.ts
+++ src/ts/classes/Grid.ts
@@ -14,14 +14,35 @@ export default class Grid {
         const allLayerItems = this.parent.children;
 
         for (let item of allLayerItems) {
-            let { x, y } = item.position;
-            let { width, height } = item.size;
-            let halfWidth = width / 2;
-            let halfHeight = height / 2;
+            const { x, y } = item.position;
+            const { width, height } = item.size;
+            const halfWidth = width / 2;
+            const halfHeight = height / 2;
+            const offset = item.style.margin;
 
-            let centerPoint = { x: x + halfWidth, y: y + halfHeight };
+            const centerPoint =       { x: x + halfWidth, y: y + halfHeight };
+            const topCenterPoint =    { x: x + halfWidth, y: y - offset };
+            const leftCenterPoint =   { x: x - offset, y: y + halfHeight };
+            const bottomCenterPoint = { x: x + halfWidth, y: y + height + offset };
+            const rightCenterPoint =  { x: x + width + offset, y: y + halfHeight }
+
+            
+            const leftTopPoint =      { x: x - offset, y: y - offset };
+            const leftBottomPoint =   { x: x - offset, y: y + height + offset };
+            
+            const rightTopPoint =     { x: x + width + offset, y: y - offset };
+            const rightBottomPoint =  { x: x + width + offset, y: y + height + offset };
 
             points.push(centerPoint);
+            points.push(topCenterPoint);
+            points.push(leftCenterPoint);
+            points.push(rightCenterPoint);
+            points.push(bottomCenterPoint);
+
+            points.push(leftTopPoint);
+            points.push(rightTopPoint);
+            points.push(leftBottomPoint);
+            points.push(rightBottomPoint);
         }
 
         return points;
@@ -34,7 +55,7 @@ export default class Grid {
     renderAt(context: CanvasRenderingContext2D) {
         this.points.forEach(point => {
             context.beginPath();
-            context.arc(point.x, point.y, 3, 0, 2 * Math.PI, false);
+            context.arc(point.x, point.y, 2, 0, 2 * Math.PI, false);
             context.fillStyle = 'magenta';
             context.strokeStyle = 'magenta';
             context.fill();
